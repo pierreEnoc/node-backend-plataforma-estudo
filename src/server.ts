21598f47eebfe543c3d7fd2ce3e8853c03e66978
pierreEnoc <pierre_enoc@yahoo.fr>
2020-08-06 20:05:21 -0300

    :sparkles reques param e query

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,21 @@
 import express from 'express';
 
 const app = express();
+app.use(express.json())
 // rotas 
-app.get('/users', (request, response) => {
+//corpo(Reuest Body): Dados para cria de um registro
+//Route Params: Identificar qual recurso eu quero atualizar ou deleter
+//Query Params: listagem paginacao, filtro,ordrnacao etc..
+
+app.post('/users', (request, response) => {
+
+    console.log(request.query);
+
     const users = [
         {name: 'Pierre', age: 38 },
         {name: 'Marcia', age: 33 },
     ];
+
     return response.json(users);
 });
 
