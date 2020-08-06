cce36374d9fef2fa1af6903824c48eb4c98734c6
pierreEnoc <pierre_enoc@yahoo.fr>
2020-08-06 19:46:02 -0300

    :sparkles route test

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,13 @@
-console.log('Hello World');
+import express from 'express';
+
+const app = express();
+// rotas 
+app.get('/users', (request, response) => {
+    const users = [
+        {name: 'Pierre', age: 38 },
+        {name: 'Marcia', age: 33 },
+    ];
+    return response.json(users);
+});
+
+app.listen(3333);
