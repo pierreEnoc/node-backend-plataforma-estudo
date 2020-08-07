0a4f30fea5814993e44a8876a1d3ca532c061a7b
pierreEnoc <pierre_enoc@yahoo.fr>
2020-08-06 22:19:41 -0300

    :sparkles create migration

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -1,13 +1,14 @@
 import knex from 'knex';
 import path from 'path';
 
+//migarations-- controlam a versão do banco de dados
 
 const db = knex({
  client: 'sqlite3',
  connection: {
      filename: path.resolve(__dirname, 'database.sqlite')
  },
-    useNullAsDefault: true,
+  useNullAsDefault: true,
 
 })
 
--- src/server.ts
+++ src/server.ts
@@ -2,14 +2,10 @@ import express from 'express';
 
 const app = express();
 app.use(express.json())
+
+app.listen(3333);
 // rotas 
 //corpo(Reuest Body): Dados para cria de um registro
 //Route Params: Identificar qual recurso eu quero atualizar ou deleter
 //Query Params: listagem paginacao, filtro,ordrnacao etc..
 
-app.get('/', (request, response) => {
-
-    return response.json({message:'Hello'});
-});
-
-app.listen(3333);
