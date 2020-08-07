4abe4a4e0e6abc5b464f3531b4ac64c866078019
pierreEnoc <pierre_enoc@yahoo.fr>
2020-08-06 23:22:49 -0300

    :sparkles add firt insert to database sqlite3

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,61 @@
 
 import express from 'express';
+import db from './database/connection';
+import convertHourToMinutes from './utils/convertHourToMinutes';
+
+
 const routes = express.Router();
 
-routes.get('/', (request, response) => {
+interface ScheduleItem {
+    week_day: number;
+    from: string;
+    to: string;
+}
 
-    return response.json({message:'Hello'});
-});
+routes.post('/classes', async (request, response) => {
+
+    const { 
+        name,
+        avatar,
+        watsapp,
+        bio,
+        subject,
+        cost,
+        schedule
+    } = request.body;
+
+    const insertedUsersId = await db('users').insert({
+        name,
+        avatar,
+        watsapp,
+        bio,
+    });
+
+    // para pegar o id
+    const user_id = insertedUsersId[0];
+
+    const insertedClassesId = await db('classes').insert({
+        subject,
+        cost,
+        user_id,
+    
+    });
+
+    const class_id = insertedUsersId[0];
+
+    const classSchedule = schedule.map((scheduleItem: ScheduleItem) => {
+        return{
+            class_id,
+            week_day: scheduleItem.week_day,
+            from: convertHourToMinutes(scheduleItem.from),
+            to: convertHourToMinutes(scheduleItem.to)
+        };
+    })
+
+    await db('class_schedule').insert(classSchedule);
+
+   return response.send();
+
+});   
 
 export default routes;
